9ba2f5e0cdc46da73be9a1387d70a737916405c6
Banjo <26153040+i74578@users.noreply.github.com>
2022-10-11 20:37:31 +0200

    Update FixWindows.ps1

--- FixWindows.ps1
+++ FixWindows.ps1
@@ -1,1 +1,5 @@
+#Disable hotkey for toggle keyboard layour
 reg add "HKCU\Keyboard Layout\toggle" /v "Layout Hotkey" /t REG_SZ /d 3 /f
+
+#Show classic right click menu in exporer
+reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
