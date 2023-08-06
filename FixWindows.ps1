24359f7cfa575c40a11b58df6fc873f698a0fac9
Banjo <26153040+i74578@users.noreply.github.com>
2023-08-06 14:20:46 +0200

    Update FixWindows.ps1

--- FixWindows.ps1
+++ FixWindows.ps1
@@ -5,7 +5,6 @@ reg add "HKCU\Keyboard Layout\toggle" /v "Layout Hotkey" /t REG_SZ /d 3 /f
 #Show classic right click menu in exporer
 reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 
-
 ####FAROESE SETTINGS
 #Adds faroese to the top on the language list and adds the correct keyboard layout
 $OldList = Get-WinUserLanguageList
@@ -27,3 +26,9 @@ Copy-UserInternationalSettingsToSystem -WelcomeScreen $True -NewUser $True
 #Sets the region formatting to faroese
 
 #Sets the "Country or region"(Windows and apps might use your country or region to give you local content) to faroese
+
+#Remove web search from start menu. Prevent accidental web search
+if( -not (Test-Path -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer)){
+  New-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer
+}
+Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer -Name "DisableSearchBoxSuggestions" -Value 1 -Type DWORD
